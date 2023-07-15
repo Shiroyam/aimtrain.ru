701625236c54b35bd5134b4c07f74c27bd60c62e
Shiroyam <shirooyam@gmail.com>
2023-07-15 20:49:38 +0300

    feat(shared/ui): add ui for avatar

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,3 +1,23 @@
+import { Avatar } from "shared/ui/avatar";
+
 export function App() {
-  return <></>;
+  return (
+    <>
+      <Avatar
+        size="xl"
+        src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/920.jpg"
+      />
+      <Avatar size="xl" />
+      <Avatar
+        size="m"
+        src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/920.jpg"
+      />
+      <Avatar size="m" />
+      <Avatar
+        size="xs"
+        src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/920.jpg"
+      />
+      <Avatar size="xs" />
+    </>
+  );
 }
