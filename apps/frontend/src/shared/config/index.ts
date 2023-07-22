db2664ad763ee5d999285eeecb9bd08b98531090
Shiroyam <shirooyam@gmail.com>
2023-07-22 13:10:27 +0300

    feat(shared/config): add config data

---
+++ apps/frontend/src/shared/config/index.ts
@@ -0,0 +1,55 @@
+/*
+ * Delay for render dot.
+ */
+export const DELAY = 500;
+
+/*
+ * Color for background dot.
+ */
+export const COLORS = [
+  "#ECDB54",
+  "#6F9FD8",
+  "#944743",
+  "#00A591",
+  "#6B5B95",
+  "#BFD641",
+  "#BC70A4",
+];
+
+/*
+ * Count of dot render.
+ */
+export const COUNT = [10, 25, 50, 100];
+
+/*
+ * Size frame render.
+ */
+export const SIZE = {
+  X: {
+    MIN: 32,
+    MAX: 880,
+  },
+  Y: {
+    MIN: 32,
+    MAX: 550,
+  },
+};
+
+/*
+ * Mode for game.
+ */
+export interface Mode {
+  id: number;
+  name: string;
+}
+
+export const MODES: Mode[] = [
+  {
+    id: 0,
+    name: "classic",
+  },
+  {
+    id: 1,
+    name: "line",
+  },
+];
