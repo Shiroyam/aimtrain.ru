87ed74a5c8844f87c83873730287dccd455d6b41
Shiroyam <shirooyam@gmail.com>
2023-07-23 17:16:32 +0300

    feat(components): add time to stat component

--- apps/frontend/src/components/stat/stat.tsx
+++ apps/frontend/src/components/stat/stat.tsx
@@ -4,15 +4,17 @@ import styles from "./stat.module.scss";
 interface StatProps {
   hits: number;
   miss: number;
+  time: number;
   className?: string;
 }
 
-export const Stat = ({ hits, miss, className }: StatProps) => {
+export const Stat = ({ hits, miss, time, className }: StatProps) => {
   const statCN = cn(styles.stat, className);
 
   return (
     <div className={statCN}>
       <div className={styles.hits}>{hits}</div>
+      <div className={styles.time}>{time}s</div>
       <div className={styles.miss}>{miss}</div>
     </div>
   );
